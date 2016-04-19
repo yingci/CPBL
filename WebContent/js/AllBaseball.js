f7b3ac15d77dfd878cf123839385d5914a2952ea
yingci <pooh811120@gmail.com>
2016-04-19 15:51:50 +0800

    CPBL
    CPBL

---
+++ WebContent/js/AllBaseball.js
@@ -0,0 +1,15 @@
+$(function() {
+  $('.toggle-nav').click(function() {
+    $('body').toggleClass('show-nav');
+     return false;
+  });
+  
+});
+
+// Toggle with hitting of ESC
+$(document).keyup(function(e) {
+	if (e.keyCode == 27) {
+   $('body').toggleClass('show-nav');
+  }
+});
+
